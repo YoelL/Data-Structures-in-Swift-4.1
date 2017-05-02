4ddce53598282a72e271a4e578c1ed2d732e3284
Yoel Lev <yoelev@Yoels-Mac-Pro.local>
2017-05-02 20:27:44 +0300

    Vertex implementation

---
+++ Vertex.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+public class Vertex{
+    
+    var key: String?
+    var neighbors: Array<Edge>
+    var visited: Bool = false
+    
+    init() {
+        self.neighbors = Array<Edge>()
+    }
+}
